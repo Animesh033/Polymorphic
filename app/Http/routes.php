b64a8fce06ad83e64110c1ece9a6fcb6f37b6718
Animesh <animesh.ultimate@gmail.com>
2019-09-10 21:42:03 +0530

    Polymorphic Relationship With Crud Operation.

---
+++ app/Http/routes.php
@@ -0,0 +1,56 @@
+<?php
+
+use App\Photo;
+use App\Staff;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/create', function () {
+    $staff = Staff::findOrFail(1);
+    $staff->photos()->create(['path'=>'example.jpg']);
+});
+
+Route::get('/read', function () {
+    $staff = Staff::findOrFail(1);
+    // dd($staff->photos);
+    foreach ($staff->photos as $photo) {
+        return $photo->path;
+    }
+});
+
+Route::get('/update', function () {
+    $staff = Staff::findOrFail(1);
+    $photo = $staff->photos()->whereId(1)->first();
+    $photo->path = "Updated example.jpg";
+    $photo->save();
+});
+
+Route::get('/delete', function () {
+    $staff = Staff::findOrFail(1);
+    // $photo = $staff->photos()->delete();
+    $photo = $staff->photos()->whereId(2)->delete();
+});
+
+Route::get('/assign', function () {
+    $staff = Staff::findOrFail(1);
+    $photo = Photo::findOrFail(4);
+    $staff->photos()->save($photo);
+});
+
+Route::get('/un-assign', function () {
+    $staff = Staff::findOrFail(1);
+    $staff->photos()->whereId(4)->update(['imageable_id' => '', 'imageable_type' => '', 'created_at' => '']);
+});
